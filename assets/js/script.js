47c82c1691b10960270bd770729f0dc7d49bdc5b
J0ly84 <you@example.com>
2023-05-28 11:48:11 +0100

    adding javascript functions to run the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
 let bullets = 10;
 let counts = 0;
+let endGame = document.getElementById("endGame");
+let result = document.getElementById("result")
 
+window.onload = function (event){
+    endGame.style.display = "none";
+}
+
+// If user successfuly shot the bird, the bird will disapear and it will be counted.
+//The bullets will increment wither the player hits a bird or missed it.
 
 window.onclick = function (event){
-    if ( bullets > 0 ){
+    if (bullets == 0) {
+        endGame.style.display = 'block' ;
+        result.innerHTML = counts;
+
+    }else if ( bullets > 0 ) {
         bullets --;
         document.getElementById("currentBullets").innerHTML = bullets;
-        
+
         if (event.target.nodeName == ("IMG")){
             document.getElementById("gun").play();
             event.target.style.display = "none";
             document.getElementById("currentScore").innerHTML = ++counts;
         } else if (event.target.nodeName != ("IMG")){
-            document.getElementById("misshot").play();
-        }
-    }
+            document.getElementById("misshot").play()
+        };
+
+    } 
 }
+
+// If the player ran out of bullets the game ends and his score is counted
