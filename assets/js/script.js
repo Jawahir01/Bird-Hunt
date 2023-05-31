2addcb1b965512e350ee4660e46487fd52b8e9fa
J0ly84 <you@example.com>
2023-06-01 00:19:01 +0100

    conflict (timer vs game) in eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,15 @@ timeOut.style.display = "none";
 
 function initializeGame(event) {
     window.addEventListener("click", shootBullet);
-    
-     
 }
-function GameTimer(event){
-    document.addEventListener("load", countdown);
+
+const timer=  () => {
+    window.addEventListener("load", countdown);
 }
 
 /**
 JS doc here
 **/
-
-//
 
 function shootBullet(event) {
     if (bullets > 0) {
@@ -37,6 +34,7 @@ function shootBullet(event) {
     if (bullets <= 0 ){
         endGame.style.display = 'block';
         result.innerHTML = counts;
+        window.removeEventListener("load", countdown);
     }
 }
 
@@ -49,9 +47,10 @@ function shootBullet(event) {
     function countdown() {
         elem.innerHTML = timeLeft;
         timeLeft--;
-        if (timeLeft == -1 ){
+        if (timeLeft <= -1 ){
             clearTimeout(timerId);
             timeOut.style.display = 'block';
+            window.removeEventListener("click", shootBullet);
         }
     }
 
@@ -70,55 +69,4 @@ function shootBullet(event) {
       setInterval(hourglass, 5000);
 
 window.addEventListener('DOMContentLoaded', initializeGame);
-window.addEventListener('DOMContentLoaded', GameTimer);
-
-
-
-
-
-// If user successfuly shot the bird, the bird will disapear and it will be counted.
-//The bullets will increment wither the player hits a bird or missed it.
-
-
-
-
-//
-
-/*let bullets = 10;
-let counts = 0;
-let endGame = document.getElementById("endGame");
-let result = document.getElementById("result")
-
-window.onload = function (event){
-    endGame.style.display = "none";
-}
-
- If user successfuly shot the bird, the bird will disapear and it will be counted.
-//The bullets will increment wither the player hits a bird or missed it.
-
-window.onclick = function (event){
-    if (bullets == 0) {
-        endGame.style.display = 'block' ;
-        result.innerHTML = counts;
-
-    }else if ( bullets > 0 ) {
-        bullets --;
-        document.getElementById("currentBullets").innerHTML = bullets;
-
-        if (event.target.nodeName == ("IMG")){
-            document.getElementById("gun").play();
-            event.target.style.display = "none";
-            document.getElementById("currentScore").innerHTML = ++counts;
-        } else if (event.target.nodeName != ("IMG")){
-            document.getElementById("misshot").play()
-        };
-
-    }
-}
-
-// If the player ran out of bullets the game ends and his score is counted*/
-// function mytest(){
-//     console.log("hello from inside mytest function");
-// }
-
-// window.addEventListener('DOMContentLoaded', mytest);
+window.addEventListener('DOMContentLoaded', timer);
