5eea746344896911a5e92405eb539b22387a2244
J0ly84 <you@example.com>
2023-05-31 15:38:00 +0100

    fixing the responsivness + adding a timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,79 @@
 let bullets = 10;
 let counts = 0;
 let endGame = document.getElementById("endGame");
+let timeOut = document.getElementById("timeOut")
 let result = document.getElementById("result");
 endGame.style.display = "none";
+timeOut.style.display = "none";
 
 function initializeGame(event) {
     window.addEventListener("click", shootBullet);
     
+     
+}
+function GameTimer(event){
+    document.addEventListener("load", countdown);
 }
 
 /**
 JS doc here
 **/
+
+//
+
 function shootBullet(event) {
-    if ( bullets > 0 ) {
-        bullets --;
+    if (bullets > 0) {
+        bullets--;
         document.getElementById("currentBullets").innerHTML = bullets;
-        if (event.target.nodeName == ("IMG")){
+        if (event.target.nodeName == ("IMG")) {
             document.getElementById("gun").play();
             event.target.style.display = "none";
             document.getElementById("currentScore").innerHTML = ++counts;
-        } else if (event.target.nodeName != ("IMG")){
+        } else if (event.target.nodeName != ("IMG")) {
             document.getElementById("misshot").play();
         }
-    } else if (bullets <= 0) {
-        endGame.style.display = 'block' ;
+    }
+
+    if (bullets <= 0 ){
+        endGame.style.display = 'block';
         result.innerHTML = counts;
     }
 }
 
 
+//  Countdown 
+    var timeLeft = 10;
+    var elem = document.getElementById('countdown-number');
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+        elem.innerHTML = timeLeft;
+        timeLeft--;
+        if (timeLeft == -1 ){
+            clearTimeout(timerId);
+            timeOut.style.display = 'block';
+        }
+    }
+
+    function hourglass() {
+        var a;
+        a = document.getElementById("div1");
+        a.innerHTML = "&#xf251;";
+        setTimeout(function () {
+            a.innerHTML = "&#xf252;";
+          }, 1000);
+        setTimeout(function () {
+            a.innerHTML = "&#xf253;";
+          }, 2000);
+      }
+      hourglass();
+      setInterval(hourglass, 5000);
+
 window.addEventListener('DOMContentLoaded', initializeGame);
+window.addEventListener('DOMContentLoaded', GameTimer);
+
+
+
 
 
 // If user successfuly shot the bird, the bird will disapear and it will be counted.
@@ -39,7 +82,7 @@ window.addEventListener('DOMContentLoaded', initializeGame);
 
 
 
-
+//
 
 /*let bullets = 10;
 let counts = 0;
@@ -70,7 +113,7 @@ window.onclick = function (event){
             document.getElementById("misshot").play()
         };
 
-    } 
+    }
 }
 
 // If the player ran out of bullets the game ends and his score is counted*/
